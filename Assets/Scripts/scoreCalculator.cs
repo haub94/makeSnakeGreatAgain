aebdeeacb5e5d18536e0a6f433fe343bc04f4515
haub94 <105038675+haub94@users.noreply.github.com>
2022-06-01 23:11:20 +0200

    Schnittstelle zu Schlange und Textfeld erstellt

--- Assets/Scripts/scoreCalculator.cs
+++ Assets/Scripts/scoreCalculator.cs
@@ -2,7 +2,7 @@
 *Name:          scoreCalculator
 *Description:   The script calculates the highscore depending on the lenght of
 *               the snake and playtime.
-*Author(s):     Markus
+*Author(s):     Markus Haubold
 *Date:          2022-05-31
 *Version:       V1.0 
 */
@@ -10,22 +10,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class scoreCalculator : MonoBehaviour
 {
-    //create snake from class Snake
     Snake snake;
-  
+    private TextMeshProUGUI scorefield;
+
+    
+    //getter and setter
+    //scorefield
+    public void setScorefield(string value) {
+        scorefield.text = value;
+    }
+    public string getScorefield() {
+        string text = "";
+
+
+        return text;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         //reference to snake object
-        snake = GameObject.Find("Snake").GetComponent<Snake> ();
+        snake = GameObject.Find("Snake").GetComponent<Snake>();
+        scorefield = GetComponent<TextMeshProUGUI>();
+        setScorefield("0");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(snake.lenght);
+        //hier die berechnung machen
+        Debug.Log("act snakelenght: " + snake.lenght);
+        setScorefield(snake.lenght.ToString());
+
+        //show actual score in gamefield
+        
+
     }
 }
