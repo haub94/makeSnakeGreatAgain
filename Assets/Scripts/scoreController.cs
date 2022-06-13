ee1b502b0c020c090663f4d9099f5b4f2faba39a
haub94 <105038675+haub94@users.noreply.github.com>
2022-06-14 01:33:07 +0200

    Übertrag in Highscoreliste funktioniert; "Sortieralgorithmus" n.i.O

--- Assets/Scripts/scoreController.cs
+++ Assets/Scripts/scoreController.cs
@@ -1,4 +1,4 @@
-/******************************************************************************
+/**********************************************************************************************************************
 Name:          scoreCalculator
 Description:   The script calculates the highscore depending on the lenght of
                the snake and playtime.
@@ -7,7 +7,8 @@ Date:          2022-05-31
 Version:       V1.0 
 TODO:          - scorefield genauso wie playername (public)
                - private array, in welches die public textdaten eingepflegt werden
-******************************************************************************/
+               - spielername aus Snake.cs ankoppeln 
+**********************************************************************************************************************/
 
 using System;
 using System.Collections;
@@ -38,7 +39,11 @@ public class scoreController : MonoBehaviour
     private int[] playerscoreAsInt = new int[6];
     //switch on for debug stuff
     private const bool debugModeOn = true;
+    public string snakePlayerName = "horstiHorst94"; //Platzhalter
+    public bool gameover;   //PLatzhalter
 
+
+    public string testScore;
 
     // Start is called before the first frame update
     void Start() {
@@ -49,27 +54,16 @@ public class scoreController : MonoBehaviour
 
         //initialize highscorelist
         //the very first run of the game => set keynames for Playerprefs
+        //check only the 1st place because if there was a first game, the score will be the 1st place
         if (PlayerPrefs.GetString("playername1") == "") {
-            String keynameName;
-            String keynameScore;
-            for (int index = 1; index < 6; index++) {
-                keynameName = "playername" + index;
-                keynameScore = "playerscore" + index;
-                PlayerPrefs.SetString(keynameName , "Wer wird Platz " + index + " belegen?!");
-                PlayerPrefs.SetString(keynameScore , "123");
+            for (byte index = 1; index < 6; index++) {
+                PlayerPrefs.SetString("playername" + index , "Wer wird Platz " + index + " belegen?!");
+                PlayerPrefs.SetString("playerscore" + index , "123");
             }
         }
-        //read values from playerprefs
-        playername1.text = PlayerPrefs.GetString("playername1");
-        playerscore1.text = PlayerPrefs.GetString("playerscore1");
-        playername2.text = PlayerPrefs.GetString("playername2");
-        playerscore2.text = PlayerPrefs.GetString("playerscore2");
-        playername3.text = PlayerPrefs.GetString("playername3");
-        playerscore3.text = PlayerPrefs.GetString("playerscore3");
-        playername4.text = PlayerPrefs.GetString("playername4");
-        playerscore4.text = PlayerPrefs.GetString("playerscore4");
-        playername5.text = PlayerPrefs.GetString("playername5");
-        playerscore5.text = PlayerPrefs.GetString("playerscore5");
+        //values from playerprefs --> playername/score.text --> playername/scoreAsInt
+        copyPlayerprefsToHighscore(true, 0);
+        setPlayerscoreAsInt(true, 0, 0);
 
         //initilize array from setPlayerscoreAsInt
         
@@ -78,7 +72,7 @@ public class scoreController : MonoBehaviour
     //getter and setter
     //scorefield
     public string getScorefield() {
-        string text = "";
+        string text = scorefield.text;
 
         return text;
     }
@@ -92,14 +86,14 @@ public class scoreController : MonoBehaviour
         
         return  returnValue;
     }
-    public void setPlayerscoreAsInt(int index, int value, bool initialize) {
+    public void setPlayerscoreAsInt(bool all, byte index, int value) {
         //int returnValue = 0;
-        if (initialize) {   //initialize array
+        if (all) {   //initialize array
             playerscoreAsInt[1] = Int32.Parse(playerscore1.text);
-            /*playerscoreAsInt[2] = Int32.Parse(playerscore2.text);
+            playerscoreAsInt[2] = Int32.Parse(playerscore2.text);
             playerscoreAsInt[3] = Int32.Parse(playerscore3.text);
             playerscoreAsInt[4] = Int32.Parse(playerscore4.text);
-            playerscoreAsInt[5] = Int32.Parse(playerscore5.text);*/
+            playerscoreAsInt[5] = Int32.Parse(playerscore5.text);
             
             //debug
             if (debugModeOn) {
@@ -109,14 +103,54 @@ public class scoreController : MonoBehaviour
                 playerscoreAsInt[3] + " | " +
                 playerscoreAsInt[4] + " | " +
                 playerscoreAsInt[5]);
-                initialize = false;
             }
-        } else {
+        } 
+        if (!all && (index > 0)) {
             playerscoreAsInt[index] = value;
         }
     }
     
     
+    private void copyPlayerprefsToHighscore(bool all, byte index) {
+        if (all) {
+            playername1.text = PlayerPrefs.GetString("playername1");
+            playerscore1.text = PlayerPrefs.GetString("playerscore1");
+            playername2.text = PlayerPrefs.GetString("playername2");
+            playerscore2.text = PlayerPrefs.GetString("playerscore2");
+            playername3.text = PlayerPrefs.GetString("playername3");
+            playerscore3.text = PlayerPrefs.GetString("playerscore3");
+            playername4.text = PlayerPrefs.GetString("playername4");
+            playerscore4.text = PlayerPrefs.GetString("playerscore4");
+            playername5.text = PlayerPrefs.GetString("playername5");
+            playerscore5.text = PlayerPrefs.GetString("playerscore5");
+        } else {
+            switch (index) {
+                case 1:
+                    playername1.text = PlayerPrefs.GetString("playername1");
+                    playerscore1.text = PlayerPrefs.GetString("playerscore1");
+                    break;
+                case 2:
+                    playername2.text = PlayerPrefs.GetString("playername2");
+                    playerscore2.text = PlayerPrefs.GetString("playerscore2");
+                    break;
+                case 3:
+                    playername3.text = PlayerPrefs.GetString("playername3");
+                    playerscore3.text = PlayerPrefs.GetString("playerscore3");
+                    break;
+                case 4:
+                    playername4.text = PlayerPrefs.GetString("playername4");
+                    playerscore4.text = PlayerPrefs.GetString("playerscore4");
+                    break;
+                case 5:
+                    playername5.text = PlayerPrefs.GetString("playername5");
+                    playerscore5.text = PlayerPrefs.GetString("playerscore5");
+                    break;
+            }
+        }
+    }
+
+
+
     //calculate the actual score (depends on snakelenght)
     private double calculate(int lenght, float playtime, int bonusFactor1, int bonusFactor2) {
         //function parameters
@@ -141,21 +175,25 @@ public class scoreController : MonoBehaviour
 
     //refresh / upddate highscore window
     public bool refreshHighscoreList() {
-        
-        
+        for (byte index = 1; index < 6; index++) {
+            if (Int32.Parse(getScorefield()) >= getPlayerscoreAsInt(index)) {
+                //sort scores if necessary
+                PlayerPrefs.SetString("playername" + index, snakePlayerName);    //name wahrscheinlich nochmal anpassen, wenn in snake vorhanden
+                PlayerPrefs.SetString("playerscore" + index, getScorefield());
+                copyPlayerprefsToHighscore(false, index);
 
-
-        return true;
+                log("highscore refresh done", Int32.Parse(getScorefield()), getPlayerscoreAsInt(index));
+                return true;
+            } 
+        }
+        return false;
     }
 
     //debug stuff
     //main debug
     private void debugArea() {
-        debugWritePlayerpref("playername1", "debugPlayername");
-        debugWritePlayerpref("playerscore1", "123456");
-
-        setPlayerscoreAsInt(0, 0, true);
-        log("playerscore: ", getPlayerscoreAsInt(1), 0);
+        log("DEBUG-MODE ACTIVE!!!", 0, 0);
+        
     }
     //write Playerprefs for debug
     private void debugWritePlayerpref(String keyname, String value) {
@@ -163,21 +201,31 @@ public class scoreController : MonoBehaviour
     }
     //log shorthand
     private void log(String message, int intValue1, int intValue2) {
-        UnityEngine.Debug.Log(message + " " + intValue1 + " " + intValue2);
+        UnityEngine.Debug.Log(message + " " + intValue1.ToString() + " " + intValue2.ToString());
     }
 
 
     // Update is called once per frame
     void Update()
     {
-       if (debugModeOn) {
-        debugArea();
-        log("DEBUG-MODE ACTIVE!!!", 0, 0);
-       }
-
         //calculate and update scorefield
         setScorefield(calculate(snake.getLenght(), 1, 1, 1).ToString());
-        
+
+
+        setScorefield(testScore); //TEST
+        //gamover = refresh highscorelist
+        if (gameover) {
+            bool done = refreshHighscoreList();
+            if (done) {
+                gameover = false;
+            }
+        }
+
+
+
+        if (debugModeOn) {
+            debugArea();
+       }   
 
     }
 }
